0c9b1e344aa20d7a64548add364780b293b30827
Mickaël Andrieu <andrieu.travail@gmail.com>
2014-03-01 05:01:59 +0100

    'foo'

---
+++ api.js
@@ -0,0 +1,37 @@
+var server = require('webserver').create(),
+    system = require('system'),
+    fs     = require('fs'),
+    port   = system.env.PORT || 8080,
+    casper = require('casper').create(),
+    utils = require('utils');
+
+var service = server.listen(port, function(request, response) {
+    console.log(request.url)
+    var method = request.method;
+    var url = request.url;
+    if (method == 'GET') {
+        // beta routing
+        if (url !== '/favicon.ico') {
+            console.log(url)
+            response.statusCode = 200;
+            response.setHeader('Content-Type', 'text/html; charset=utf-8');
+            var path = 'pages/'+url+'.html';
+            if(fs.isFile(path)) {
+                response.write(fs.read('pages/'+url+'.html'));
+            }else {
+                response.write(fs.read('pages/index.html'));
+            }
+
+            response.close();
+        }
+
+    } else if (method == 'POST') {
+         response.statusCode = 200;
+        response.setHeader('Content-Type', 'text/html; charset=utf-8');
+        response.write('APPEL EN POST, TODO');
+        response.close();
+    }
+
+});
+
+if(service) console.log("server started - http://localhost:" + server.port);
